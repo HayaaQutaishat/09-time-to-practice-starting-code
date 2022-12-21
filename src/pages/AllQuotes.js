57c4db8f6b623fe72c5d0ace087f23a806b4d65e
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-21 16:57:56 +0100

    Handling Loading/Errors, and rendering actual data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,12 +1,34 @@
 import QuoteList from "../components/quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-  { id: "q1", text: "Learning React is Fun!", author: "Hayaa" },
-  { id: "q2", text: "Learning React is Great!", author: "Max" },
-];
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import { useEffect } from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const { sendRequest, status, data, error } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!data || data.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={data} />;
 };
 
 export default AllQuotes;
