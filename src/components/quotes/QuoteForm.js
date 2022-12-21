7a33b4f76161830df360b0971b2ee91f6b90ccf0
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-21 11:53:56 +0100

    Implementing Programmatic (Imperative) Navigation

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,15 +1,17 @@
-import { useRef } from 'react';
-
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const history = useHistory();
 
   function submitFormHandler(event) {
     event.preventDefault();
+    history.push("/quotes");
 
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
@@ -29,15 +31,15 @@ const QuoteForm = (props) => {
         )}
 
         <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
+          <label htmlFor="author">Author</label>
+          <input type="text" id="author" ref={authorInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          <label htmlFor="text">Text</label>
+          <textarea id="text" rows="5" ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button className="btn">Add Quote</button>
         </div>
       </form>
     </Card>
