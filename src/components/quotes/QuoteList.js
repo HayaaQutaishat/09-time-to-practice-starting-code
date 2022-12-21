e2f0ea4995cc15c063fc1f91a775ee314308faf2
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-21 14:14:50 +0100

    Working with Query Parameters

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,26 @@
-import { Fragment } from 'react';
-
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import { Fragment } from "react";
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+import { useLocation, useHistory } from "react-router-dom";
 
 const QuoteList = (props) => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAsc = queryParams.get("sort") === "asc";
+
+  const toggleSortHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAsc ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={toggleSortHandler}>
+          Sort {isSortingAsc ? "Descedning" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
