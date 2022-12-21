1cc99456b14478839b9e4bf1b358176839f3150f
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-21 10:59:35 +0100

    Passing the New Quote Data into Parent Component

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,6 +1,9 @@
 import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
-  return <QuoteForm />;
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
