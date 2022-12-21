09dbd3d0450f7e405b8467f730c73154b6992c55
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-21 14:59:09 +0100

    Using "useRouteMatch" for nested routes paths

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const QuoteDetail = () => {
   const params = useParams();
+  const match = useRouteMatch();
+
+  console.log(match);
+
   const DUMMY_QUOTES = [
     { id: "q1", text: "Learning React is Fun!", author: "Hayaa" },
     { id: "q2", text: "Learning React is Great!", author: "Max" },
@@ -22,14 +26,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
