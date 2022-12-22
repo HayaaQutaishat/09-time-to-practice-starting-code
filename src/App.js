1242410e352f70d34a2c4cfde7099eed680dd78c
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-12-22 16:30:44 +0100

    Adding lazy loading for other components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Suspense } from "react";
 
 import { Switch, Route, Redirect } from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-// import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
 import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
 
 function App() {
   return (
